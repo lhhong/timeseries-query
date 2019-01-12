9c40e5022fe0731793ec2a49ebaeeafee3ecd291
lhhong <heok.hong.low@gmail.com>
2019-01-12 12:36:26 +0800

    Add GETSET

--- pkg/querycache/basicOp.go
+++ pkg/querycache/basicOp.go
@@ -23,6 +23,7 @@ func (cs CacheStore) SetBytes(key string, val []byte) error {
 
 	conn := cs.redisPool.Get()
 	defer conn.Close()
+
 	_, err := conn.Do("SET", fmt.Sprintf("%s/%s", cs.env, key), val, "EX", cacheExpiry)
 	if err != nil {
 		return err
@@ -33,7 +34,20 @@ func (cs CacheStore) SetBytes(key string, val []byte) error {
 func (cs CacheStore) Delete(key string) (int, error) {
 	conn := cs.redisPool.Get()
 	defer conn.Close()
+
 	res, err := redis.Int(conn.Do("DEL", fmt.Sprintf("%s/%s", cs.env, key)))
+	if err != nil {
+		return res, err
+	}
+	return res, nil
+}
+
+func (cs CacheStore) GetsetBytes(key string, val []byte) ([]byte, error) {
+
+	conn := cs.redisPool.Get()
+	defer conn.Close()
+
+	res, err := redis.Bytes(conn.Do("GETSET", fmt.Sprintf("%s/%s", cs.env, key), val))
 	if err != nil {
 		return res, err
 	}
--- pkg/querycache/basicOp_test.go
+++ pkg/querycache/basicOp_test.go
@@ -15,42 +15,57 @@ func getCacheStore() *CacheStore {
 
 func TestCacheStore_SetGetDelBytes(t *testing.T) {
 	type args struct {
-		key string
-		val []byte
+		key       string
+		valSet    []byte
+		valGetset []byte
 	}
 	tests := []struct {
-		name       string
-		args       args
-		wantGet    []byte
-		wantDel    int
-		wantSetErr bool
-		wantGetErr bool
-		wantDelErr bool
+		name          string
+		args          args
+		wantGetset    []byte
+		wantGet       []byte
+		wantDel       int
+		wantSetErr    bool
+		wantGetsetErr bool
+		wantGetErr    bool
+		wantDelErr    bool
 	}{
 		{
-			name:       "regular",
-			args:       args{"regular", []byte("regular-value")},
-			wantGet:    []byte("regular-value"),
-			wantDel:    1,
-			wantSetErr: false,
-			wantGetErr: false,
-			wantDelErr: false,
+			name:          "regular",
+			args:          args{"regular", []byte("regular-value1"), []byte("regular-value2")},
+			wantGetset:    []byte("regular-value1"),
+			wantGet:       []byte("regular-value2"),
+			wantDel:       1,
+			wantSetErr:    false,
+			wantGetsetErr: false,
+			wantGetErr:    false,
+			wantDelErr:    false,
 		},
 		{
-			name:       "with-space",
-			args:       args{"with-space", []byte("spaced value")},
-			wantGet:    []byte("spaced value"),
-			wantDel:    1,
-			wantSetErr: false,
-			wantGetErr: false,
-			wantDelErr: false,
+			name:          "with-space",
+			args:          args{"with-space", []byte("spaced value 1"), []byte("spaced value 2")},
+			wantGetset:    []byte("spaced value 1"),
+			wantGet:       []byte("spaced value 2"),
+			wantDel:       1,
+			wantSetErr:    false,
+			wantGetsetErr: false,
+			wantGetErr:    false,
+			wantDelErr:    false,
 		},
 	}
 	cs := getCacheStore()
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := cs.SetBytes(tt.args.key, tt.args.val); (err != nil) != tt.wantSetErr {
+			if err := cs.SetBytes(tt.args.key, tt.args.valSet); (err != nil) != tt.wantSetErr {
 				t.Errorf("CacheStore.SetBytes() error = %v, wantSetErr %v", err, tt.wantSetErr)
+			}
+			gotGetset, err := cs.GetsetBytes(tt.args.key, tt.args.valGetset)
+			if (err != nil) != tt.wantGetsetErr {
+				t.Errorf("CacheStore.GetsetBytes() error = %v, wantGetsetErr %v", err, tt.wantGetsetErr)
+				return
+			}
+			if !reflect.DeepEqual(gotGetset, tt.wantGetset) {
+				t.Errorf("CacheStore.GetsetBytes() gotGetset = %v, wantGetset %v", gotGetset, tt.wantGetset)
 			}
 			gotGet, err := cs.GetBytes(tt.args.key)
 			if (err != nil) != tt.wantGetErr {

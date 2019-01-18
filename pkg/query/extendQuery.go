03ef964d82a85d4b445dcb21aa21072d0835a7bc
lhhong <heok.hong.low@gmail.com>
2019-01-18 16:40:14 +0800

    Calculate width cutoffs

--- pkg/query/extendQuery.go
+++ pkg/query/extendQuery.go
@@ -88,21 +88,65 @@ func getNextSection(repo *repository.Repository, prevSection *repository.Section
 }
 
 func withinWidthAndHeight(partialMatch *PartialMatch, nextSection *repository.SectionInfo, queryWidth int64, queryHeight float64) bool {
+
+	// TODO Export to parameters
+
+	widthRatioExponent := 0.3
+	widthRatioMultiplier := 1.8
+	widthMinimumCutoff := 0.3
+
+	heightRatioExponent := 0.3
+	heightRatioMultiplier := 1.0
+	heightMinimumCutoff := 0.3
+
+	widthLowerLimit, widthUpperLimit := getWidthOrHeightLimits(float64(queryWidth), float64(partialMatch.PrevWidth),
+		queryHeight, float64(partialMatch.LastSection.Width), widthRatioExponent, widthRatioMultiplier, widthMinimumCutoff)
+	heightLowerLimit, heightUpperLimit := getWidthOrHeightLimits(queryHeight, partialMatch.PrevHeight, float64(queryWidth),
+		partialMatch.LastSection.Height, heightRatioExponent, heightRatioMultiplier, heightMinimumCutoff)
+
+	if float64(nextSection.Width) < widthLowerLimit || float64(nextSection.Width) > widthUpperLimit {
+		return false
+	}
+	if nextSection.Height < heightLowerLimit || nextSection.Height > heightUpperLimit {
+		return false
+	}
+	return true
+}
+
+// Refer to commented out implementation below for explanation
+func getWidthOrHeightLimits(queryLength float64, prevQueryLength float64, oppQueryLength float64,
+	prevDataLength float64, ratioExponent float64, ratioMultiplier float64, minimumCutoff float64) (float64, float64) {
+
+	queryRatio := queryLength / prevQueryLength
+	ratioLimit := ratioMultiplier * math.Pow((oppQueryLength/queryLength), ratioExponent)
+	lowerRatioLimit := queryRatio * prevDataLength / (ratioLimit + 1)
+	lowerCutoffLimit := (queryRatio - minimumCutoff) * prevDataLength
+	upperRatioLimit := queryRatio * prevDataLength * (ratioLimit + 1)
+	upperCutoffLimit := (queryRatio + minimumCutoff) * prevDataLength
+
+	return math.Min(lowerRatioLimit, lowerCutoffLimit), math.Max(upperRatioLimit, upperCutoffLimit)
+}
+
+// ORIGINAL WIDTH HEIGHT MATCHING ALGO
+// FOR REFERENCE WRT LIMIT CALCULATION
+//
+// Main Idea:
+// 1. If query draws height to be wider than width, height is more important and width is less important
+//			Prioritize height ratio and make width ratio more lenient
+// 2. If height difference with previous section is huge, there may be more distortion from user drawing
+//			eg, 4x height difference vs 5x height difference is not sigificant visually but is huge in absolute values
+//			Weigh the ratio based on difference factor
+// 3. The previous approach leads to 2 consecutive sections having the same height will have small margin of error
+//			Apply minimum cut off
+// 4. Height is more important than width
+
+/*
+func withinWidthAndHeight(partialMatch *PartialMatch, nextSection *repository.SectionInfo, queryWidth int64, queryHeight float64) bool {
 	queryWidthRatio := float64(queryWidth) / float64(partialMatch.PrevWidth)
 	queryHeightRatio := queryHeight / partialMatch.PrevHeight
 
 	dataWidthRatio := float64(nextSection.Width) / float64(partialMatch.LastSection.Width)
 	dataHeightRatio := nextSection.Height / partialMatch.LastSection.Height
-
-	// Main Idea:
-	// 1. If query draws height to be wider than width, height is more important and width is less important
-	//			Prioritize height ratio and make width ratio more lenient
-	// 2. If height difference with previous section is huge, there may be more distortion from user drawing
-	//			eg, 4x height difference vs 5x height difference is not sigificant visually but is huge in absolute values
-	//			Weigh the ratio based on difference factor
-	// 3. The previous approach leads to 2 consecutive sections having the same height will have small margin of error
-	//			Apply minimum cut off
-	// 4. Height is more important than width
 
 	limitMultiplier := queryHeight / float64(queryWidth)
 
@@ -139,6 +183,7 @@ func withinWidthAndHeight(partialMatch *PartialMatch, nextSection *repository.Se
 	}
 	return true
 }
+*/
 
 func inRelevantClusters(repo *repository.Repository, nextSection *repository.SectionInfo, relevantClusters []int) bool {
 	for _, clusterIndex := range relevantClusters {
--- pkg/query/handler.go
+++ pkg/query/handler.go
@@ -13,59 +13,115 @@ import (
 
 type Updates struct {
 	IsFinal bool
-	Query   []*repository.Values
+	Query   []repository.Values
 }
 
-func PublishUpdates(cs *querycache.CacheStore, sessionID string, query []*repository.Values) {
+func PublishUpdates(cs *querycache.CacheStore, sessionID string, query []repository.Values) {
 	var buf bytes.Buffer
 	enc := gob.NewEncoder(&buf)
 	enc.Encode(Updates{IsFinal: false, Query: query})
 	cs.Publish(sessionID, buf.Bytes())
 }
 
-func FinalizeQuery(repo *repository.Repository, cs *querycache.CacheStore, sessionID string, query []*repository.Values) {
+func FinalizeQuery(repo *repository.Repository, cs *querycache.CacheStore, sessionID string, query []repository.Values) []*PartialMatch {
 	var buf bytes.Buffer
 	enc := gob.NewEncoder(&buf)
 	enc.Encode(Updates{IsFinal: true, Query: nil})
+
+	// TODO create proper return data type
+	resChan := make(chan []*PartialMatch)
 	cs.Subscribe(sessionID+"FINAL", func(conn redis.Conn, dataChan chan []byte) {
 		defer cs.Unsubscribe(conn)
 		data := <-dataChan
 		dec := gob.NewDecoder(bytes.NewReader(data))
 		var matches []*PartialMatch
 		dec.Decode(matches)
-		finalize(repo, query, matches)
+		resChan <- finalize(repo, query, matches)
 	})
 	cs.Publish(sessionID, buf.Bytes())
+	res := <-resChan
+	return res
 }
 
 func StartContinuousQuery(repo *repository.Repository, cs *querycache.CacheStore, sessionID string) {
 
 	cs.Subscribe(sessionID, func(conn redis.Conn, dataChan chan []byte) {
 		defer cs.Unsubscribe(conn)
+		var matches []*PartialMatch
+		sectionsMatched := 0
+		// TODO: timeout event if no final received
 		for {
 			data := <-dataChan
 			dec := gob.NewDecoder(bytes.NewReader(data))
 			var query Updates
 			dec.Decode(&query)
 			if query.IsFinal {
-				prepareFinalize()
+				prepareFinalize(cs, sessionID, matches)
 				return
 			}
-			handleUpdate(query.Query)
+			handleUpdate(repo, &matches, &sectionsMatched, query.Query)
 		}
 	})
 }
 
-func prepareFinalize() {
+func prepareFinalize(cs *querycache.CacheStore, sessionID string, matches []*PartialMatch) {
 
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	enc.Encode(matches)
+	cs.Publish(sessionID+"FINAL", buf.Bytes())
 }
 
-func handleUpdate(query []*repository.Values) {
+func handleUpdate(repo *repository.Repository, matches *[]*PartialMatch, sectionsMatched *int, query []repository.Values) {
 
+	//Replace with alternative smoothing, eg paper.simplify
+	datautils.Smooth(query, 2, 1)
+	datautils.Smooth(query, 3, 2)
+
+	sections := datautils.ConstructSectionsFromPointsAbsoluteMinHeight(query, 0.5)
+	if len(sections) < 3 {
+		// Not ready for query yet
+		return
+	}
+
+	if *sectionsMatched == 0 {
+
+		// TODO Abstract this whole portion wth InstantQuery
+
+		// TODO Change stocks to generic groupname
+		centroids, err := repo.GetClusterCentroids("stocks", getSign(sections[1].Points))
+		if err != nil {
+			log.Println("Error getting centroids")
+			log.Println(err)
+		}
+
+		relevantClusters := getRelevantClusters(sections[1].Points, centroids)
+		width, height := getWidthAndHeight(sections[1].Points)
+		sign := getSign(sections[1].Points)
+		for _, cluster := range relevantClusters {
+			// TODO Change stocks to generic groupname
+			members, err := repo.GetMembersOfCluster("stocks", sign, cluster)
+			if err != nil {
+				log.Println("Error retriving members of cluster")
+				log.Println(err)
+			}
+			for _, member := range members {
+				*matches = append(*matches, getPartialMatch(repo, member, width, height))
+			}
+		}
+		*sectionsMatched = 1
+	}
+	if len(sections)-2 > *sectionsMatched {
+		log.Printf("extending query, i=%d", *sectionsMatched)
+		*matches = ExtendQuery(repo, *matches, sections[*sectionsMatched].Points)
+		*sectionsMatched++
+	}
 }
 
-func finalize(repo *repository.Repository, query []*repository.Values, matches []*PartialMatch) {
+// TODO create return data type
+func finalize(repo *repository.Repository, query []repository.Values, matches []*PartialMatch) []*PartialMatch {
 
+	return nil
 }
 
 func HandleInstantQuery(repo *repository.Repository, groupname string, points []repository.Values) []*PartialMatch {
